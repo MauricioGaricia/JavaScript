6fcba048d6e1673a8abb9b725dee76c8504913f0
Andres Mauricio Garcia Caicedo <82051176+MauricioGaricia@users.noreply.github.com>
2023-06-04 14:47:52 -0500

    Update calculadora.js

--- Calculadora/calculadora.js
+++ Calculadora/calculadora.js
@@ -1,22 +1,27 @@
 let string = "";
 let buttons = document.querySelectorAll("button");
-Array.from(buttons).forEach((button)=>{
-    button.addEventListener("click", (e)=>{
-        if(e.target.innerHTML == "="){
-            string = eval(string);
-            document.querySelector("textarea").innerText = string;
+Array.from(buttons).forEach((button) => {
+  button.addEventListener("click", (e) => {
+    try {
+      if (e.target.innerHTML === "=") {
+        string = eval(string);
+        if (isNaN(string) || !isFinite(string)) {
+          throw new Error("Resultado inválido");
         }
-        else if(e.target.innerHTML == "AC"){
-            string = "";
-            document.querySelector("textarea").innerText = string;
-        }
-        else if(e.target.innerHTML == "←"){
-            string = string.slice(0,1)
-            document.querySelector("textarea").innerText = string;
-        }
-        else{
-            string = string + e.target.innerText;
-            document.querySelector("textarea").innerText = string;
-        }
-    })
-})
+        document.querySelector("textarea").textContent = string;
+      } else if (e.target.innerHTML === "AC") {
+        string = "";
+        document.querySelector("textarea").textContent = string;
+      } else if (e.target.innerHTML === "←") {
+        string = string.slice(0, -1);
+        document.querySelector("textarea").textContent = string;
+      } else {
+        string = string + e.target.innerText;
+        document.querySelector("textarea").textContent = string;
+      }
+    }  catch (error) {
+        console.error(error);
+        document.querySelector("textarea").textContent = "Math ERROR";
+      }
+    });
+  });
